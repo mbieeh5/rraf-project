9c2f39a240584f5a3ad24534e7c006e3d9487b2b
mbieeh5 <mbieeh5@gmail.com>
2024-03-05 10:08:09 +0700

    update API reset Absen

--- pages/api/reset-absen.js
+++ pages/api/reset-absen.js
@@ -2,7 +2,7 @@
 
 
 export default function handler(req, res) {
-    const dbd = ref(getDatabase());
+    //const dbd = ref(getDatabase());
     if(req.method === 'POST'){
         const {data} = req.body;
         res.status(200).json({message: data.noHPDB});
